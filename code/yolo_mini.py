d3dd960014e36c6759bb27ba8cd84291b2db5393
MukhammadjonArabov <muhammadjonarabov04@gmail.com>
2025-11-10 11:15:38 +0500

    yolo first project

---
+++ code/yolo_mini.py
@@ -0,0 +1,33 @@
+from ultralytics import YOLO
+import cv2
+
+model = YOLO("yolov8n.pt")
+
+cap = cv2.VideoCapture('../test.mp4')
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    results = model(frame, stream=True)
+
+    for r in results:
+        for box in r.boxes:
+            x1, y1, x2, y2 = map(int, box.xyxy[0])
+            cls = int(box.cls[0])
+            conf = float(box.conf[0])
+            label = model.names[cls]
+
+            cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+            cv2.putText(frame, f"{label} {conf:.2f}",
+                        (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX,
+                        0.7, (0, 255, 0), 2)
+
+    cv2.imshow("YOLO + OpenCV", frame)
+
+    if cv2.waitKey(1) & 0xFF == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
